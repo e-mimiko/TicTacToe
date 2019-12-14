97f96151d527388d8309afc85af3413433e1761e
Emi Mimiko <emimiko@outlook.com>
2019-12-14 15:47:47 -0800

    2 versions of input function

--- Tictactoe.py
+++ Tictactoe.py
@@ -1,3 +1,40 @@
-Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:18:55) [MSC v.1900 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
+#x and o
+
+import random
+
+def play():
+    playOrNot=input("This is a game of Xs and Os, would you like to play? Yes/No --> ")
+    if playOrNot=="Yes":
+        playerChoice=input("Great! Which letter would you like?\nX or O --> ")
+        while playerChoice != "X" or "O":
+            print("This is not a valid input. NOTE: letters are case sensitive")
+            playerChoice=input("Great! Which letter would you like?\nX or O --> ")
+        return playerChoice
+    elif playOrNot=="No":
+        print("Thats Ok! Come back again another time. Bye!")
+    else:
+        print("This is not a valid Input. NOTE: letters are case sensitive")
+        playerChoice=input("Great! Which letter would you like?\nX or O --> ")
+        return playerChoice
+
+
+def play():
+    playOrNot=input("This is a game of Xs and Os, would you like to play? Yes/No --> ")
+    while playOrNot != "Yes" or "No":
+        print("This is not a valid input. NOTE: Letters are case sensitive")
+        playOrNot=input("This is a game of Xs and Os, would you like to play? Yes/No --> ")
+    playerChoice=input("Great! Which letter would you like?\nX or O --> ")
+    while playerChoice != "X" or "O":
+        print("This is not a valid input. NOTE: Letters are case sensitive. Try again")
+        playerChoice=input("Which letter would you like?\nX\nO\n--> ")
+    return playerChoice
+        
+
+        
+
+
+            
+
+        
+
+    
