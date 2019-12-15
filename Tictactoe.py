cc0bc106bccd91a2d5c61a8887cd3445c5c9fe13
Emi Mimiko <emimiko@outlook.com>
2019-12-14 16:19:39 -0800

    Completed welcome message and user validation

--- Tictactoe.py
+++ Tictactoe.py
@@ -2,34 +2,22 @@
 
 import random
 
-def play():
-    playOrNot=input("This is a game of Xs and Os, would you like to play? Yes/No --> ")
-    if playOrNot=="Yes":
-        playerChoice=input("Great! Which letter would you like?\nX or O --> ")
-        while playerChoice != "X" or "O":
-            print("This is not a valid input. NOTE: letters are case sensitive")
-            playerChoice=input("Great! Which letter would you like?\nX or O --> ")
-        return playerChoice
-    elif playOrNot=="No":
-        print("Thats Ok! Come back again another time. Bye!")
-    else:
-        print("This is not a valid Input. NOTE: letters are case sensitive")
-        playerChoice=input("Great! Which letter would you like?\nX or O --> ")
-        return playerChoice
-
 
 def play():
     playOrNot=input("This is a game of Xs and Os, would you like to play? Yes/No --> ")
-    while playOrNot != "Yes" or "No":
-        print("This is not a valid input. NOTE: Letters are case sensitive")
+    while playOrNot != "Yes" and playOrNot != "No":
+        print("\nThis is not a valid input. NOTE: Letters are case sensitive")
         playOrNot=input("This is a game of Xs and Os, would you like to play? Yes/No --> ")
-    playerChoice=input("Great! Which letter would you like?\nX or O --> ")
-    while playerChoice != "X" or "O":
-        print("This is not a valid input. NOTE: Letters are case sensitive. Try again")
-        playerChoice=input("Which letter would you like?\nX\nO\n--> ")
-    return playerChoice
+    if playOrNot == "No":
+        print ("\nYou chose No. Bye!")
+    else:  
+        playerChoice=input("\nGreat! Which letter would you like?\n\n\tX or O \n--> ")
+        while playerChoice != "X" and playerChoice != "O":
+            print("This is not a valid input. NOTE: Letters are case sensitive. Try again")
+            playerChoice=input("Which letter would you like?\n\n\tX or O \n--> ")
+        return playerChoice
         
-
+play()
         
 
 
