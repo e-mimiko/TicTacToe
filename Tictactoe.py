d93b759f0d3db11e44bcdac8c978bcf153f4f6c3
Emi Mimiko <emimiko@outlook.com>
2019-12-17 10:21:08 -0800

    Added the option for user to choose board size

--- Tictactoe.py
+++ Tictactoe.py
@@ -1,6 +1,9 @@
 #x and o
 
 import random
+mat=[]
+boardSize=["3","4","5"]
+
 
 def play():
     playOrNot=input("This is a game of Xs and Os, would you like to play? Yes/No --> ")
@@ -14,14 +17,21 @@ def play():
         while playerChoice != "X" and playerChoice != "O":
             print("This is not a valid input. NOTE: Letters are case sensitive. Try again")
             playerChoice=input("Which letter would you like?\n\n\tX or O \n--> ")
-        return playerChoice
+        boardChoice=input("\nGreat! What size of board do you want? 3 to 5 inclusive.\n--> ")
+        while boardChoice not in boardSize:
+            print("This is not a valid input. NOTE: Enter a valid number.")
+            boardChoice=input("\nWhat size of board do you want? 3 to 5 inclusive.\n--> ")
+        return playerChoice, boardChoice
         
-play()
+
         
 
 def Game():
-    playerChoice=play()
+    playerChoice,boardChoice=play()
+    print (playerChoice)
+    print (boardChoice)
 
+Game()
 '''
 for r in range (3):
     roll=[]
