49d011e1f166da5df9c36f94fb72f27434806f52
Emi Mimiko <emimiko@outlook.com>
2019-12-21 23:14:39 -0800

    Created exception for user row/column & made board

--- Tictactoe.py
+++ Tictactoe.py
@@ -5,7 +5,7 @@ mat=[]
 boardSize=["3","4","5"]
 
 
-def play():
+def game_open():
     playOrNot=input("This is a game of Xs and Os, would you like to play? Yes/No --> ")
     while playOrNot != "Yes" and playOrNot != "No":
         print("\nThis is not a valid input. NOTE: Letters are case sensitive")
@@ -23,29 +23,74 @@ def play():
             boardChoice=input("\nWhat size of board do you want? 3 to 5 inclusive.\n--> ")
         return playerChoice, boardChoice
         
-
         
 
-def Game():
-    playerChoice,boardChoice=play()
-    print (playerChoice)
-    print (boardChoice)
+def make_board():
+    playerChoice,boardChoice=game_open()
+    boardChoice=int(boardChoice)
 
-Game()
-'''
-for r in range (3):
-    roll=[]
-    for c in range(3):
-        roll.append("?")
-    mat.append(roll)
-#print (mat)
+    for r in range (boardChoice):
+        roll=[]
+        for c in range(boardChoice):
+            roll.append("?")
+        mat.append(roll)
+    print (mat)
 
 
-for i in range(3):
-    for h in range (3):
-        print ("\t",mat[i][r],"\t",end="")
+
+
+
+def play():
+    make_board()
+    while True:
+        try:
+            row,column = input("Enter the ROW, then a SPACE, then COLUMN you'd like to play e.g. 2 1 : ").split()
+        except ValueError:
+            print("\nPlease enter a SPACE between the row e.g. 2 3")
+            continue
+        row = int(row)
+        column = int(column)
+        if row > len(mat)-1 or column > len(mat)-1:
+            print("\nEnter a valid Row and Column")
+            continue
+        break
+    
+
+
+
+play()
+
+#print board    
+'''    
+print (mat)
+print(len(mat))
+mat[0][1]="X"
+mat[2][1]="O"
+for i in mat:
+    for x in i:
+        print("\t", x, end="")
     print()
+    
+'''   
+
+
+##old printboard
 '''
-        
+make_board()
+
+
+def print_board()
+    for i in range(boardChoice):
+        for h in range (boardChoice):
+            print ("\t",mat[i][r],end="")
+        print()
+
+print_board()
+
+'''
+
+
+
+
 
     
